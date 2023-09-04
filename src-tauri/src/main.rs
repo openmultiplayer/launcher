7f102a86a5227fb90cc9cfb2e665d5e919847a8d
iAmir <hhm6@yahoo.com>
2023-09-04 10:34:54 +0330

    add pinging functionality to rust backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,10 @@
 // use serde_json::json;
 mod query;
 
+use std::time::Instant;
+use tauri::Manager;
+use tauri::PhysicalSize;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 async fn request_server_info(ip: &str, port: i32) -> Result<String, String> {
@@ -60,8 +64,20 @@ async fn request_server_is_omp(ip: &str, port: i32) -> Result<String, String> {
     }
 }
 
-use tauri::Manager;
-use tauri::PhysicalSize;
+#[tauri::command]
+async fn ping_server(ip: &str, port: i32) -> Result<u32, String> {
+    match query::Query::new(ip, port).await {
+        Ok(q) => {
+            let _ = q.send('p').await;
+            let before = Instant::now();
+            match q.recv().await {
+                Ok(_p) => Ok(before.elapsed().as_millis() as u32),
+                Err(e) => Err(e.to_string()),
+            }
+        }
+        Err(e) => Err(e.to_string()),
+    }
+}
 
 fn main() {
     tauri::Builder::default()
@@ -76,7 +92,8 @@ fn main() {
             request_server_info,
             request_server_players,
             request_server_rules,
-            request_server_is_omp
+            request_server_is_omp,
+            ping_server
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
