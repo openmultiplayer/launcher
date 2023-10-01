308ed455d10677a1a65a361436351cedb8fd2c6d
iAmir <hhm6@yahoo.com>
2023-10-01 09:34:03 +0330

    create tab views separately

---
+++ src/containers/MainBody/ServerList/Tabs/Favorites.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useMemo } from "react";
+import { useQuery } from "../../../../hooks/query";
+import {
+  useFavServersStore,
+  useTempServersStore,
+} from "../../../../states/servers";
+import { SearchData, Server } from "../../../../utils/types";
+import List from "../List";
+import ServerItem from "./../Item";
+
+interface IProps {
+  searchData: SearchData;
+}
+
+const Favorites = (props: IProps) => {
+  const { startQuery, stopQuery } = useQuery();
+  const { favorites, updateInFavoritesList } = useFavServersStore();
+  const { selected, setSelected } = useTempServersStore();
+
+  useEffect(() => {
+    return () => {
+      stopQuery();
+      setSelected(undefined);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (selected) {
+      updateInFavoritesList(selected);
+    }
+  }, [selected]);
+
+  const list = useMemo(() => {
+    const { ompOnly, nonEmpty, query } = props.searchData;
+    const list = favorites.filter((server) => {
+      const ompCheck = ompOnly ? server.usingOmp === true : true;
+      const nonEmptyCheck = nonEmpty ? server.playerCount > 0 : true;
+
+      return (
+        ompCheck &&
+        nonEmptyCheck &&
+        server.hostname.toLowerCase().includes(query.toLowerCase())
+      );
+    });
+
+    return list;
+  }, [
+    props.searchData.query,
+    props.searchData.ompOnly,
+    props.searchData.nonEmpty,
+    favorites,
+  ]);
+
+  const onSelect = (server: Server) => {
+    stopQuery();
+    setSelected(server);
+    startQuery(server);
+  };
+
+  return (
+    <List
+      data={list}
+      renderItem={(item, index) => (
+        <ServerItem
+          isSelected={
+            selected
+              ? selected.ip === item.ip && selected.port === item.port
+              : false
+          }
+          server={item}
+          index={index}
+          onSelect={(server) => onSelect(server)}
+        />
+      )}
+    />
+  );
+};
+
+export default Favorites;
---
+++ src/containers/MainBody/ServerList/Tabs/Internet.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useMemo } from "react";
+import { useQuery } from "../../../../hooks/query";
+import { useTempServersStore } from "../../../../states/servers";
+import { SearchData, Server } from "../../../../utils/types";
+import List from "../List";
+import ServerItem from "./../Item";
+
+interface IProps {
+  searchData: SearchData;
+}
+
+const Internet = (props: IProps) => {
+  const { startQuery, stopQuery } = useQuery();
+  const { selected, servers, setSelected } = useTempServersStore();
+
+  useEffect(() => {
+    return () => {
+      stopQuery();
+      setSelected(undefined);
+    };
+  }, []);
+
+  const list = useMemo(() => {
+    const { ompOnly, nonEmpty, query } = props.searchData;
+    const list = servers.filter((server) => {
+      const ompCheck = ompOnly ? server.usingOmp === true : true;
+      const nonEmptyCheck = nonEmpty ? server.playerCount > 0 : true;
+
+      return (
+        ompCheck &&
+        nonEmptyCheck &&
+        server.hostname.toLowerCase().includes(query.toLowerCase())
+      );
+    });
+
+    return list;
+  }, [
+    props.searchData.query,
+    props.searchData.ompOnly,
+    props.searchData.nonEmpty,
+    servers,
+  ]);
+
+  const onSelect = (server: Server) => {
+    stopQuery();
+    setSelected(server);
+    startQuery(server);
+  };
+
+  return (
+    <List
+      data={list}
+      renderItem={(item, index) => (
+        <ServerItem
+          isSelected={
+            selected
+              ? selected.ip === item.ip && selected.port === item.port
+              : false
+          }
+          server={item}
+          index={index}
+          onSelect={(server) => onSelect(server)}
+        />
+      )}
+    />
+  );
+};
+
+export default Internet;
