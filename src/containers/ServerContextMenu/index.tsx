18a86639129d051744c53e0605ec1e02b33b856e
iAmir <hhm6@yahoo.com>
2023-10-05 08:20:22 +0330

    able to close server context menu now

--- src/containers/ServerContextMenu/index.tsx
+++ src/containers/ServerContextMenu/index.tsx
@@ -1,13 +1,14 @@
 import { useContext, useState } from "react";
-import { Pressable, View } from "react-native";
+import { Pressable, View, useWindowDimensions } from "react-native";
 import Text from "../../components/Text";
 import { useContextMenu } from "../../states/contextMenu";
 import { usePersistentServersStore } from "../../states/servers";
 import { ThemeContext } from "./../../contexts/theme";
 
 const ContextMenu = () => {
+  const { width, height } = useWindowDimensions();
   const { theme } = useContext(ThemeContext);
-  const { visible, position, server } = useContextMenu();
+  const { visible, position, server, hide } = useContextMenu();
   const { addToFavorites } = usePersistentServersStore();
   const [favBtnBgCol, setFavBtnBgCol] = useState(
     theme.listHeaderBackgroundColor
@@ -18,37 +19,58 @@ const ContextMenu = () => {
       <View
         style={{
           position: "absolute",
-          top: position.y,
-          left: position.x,
-          borderRadius: 4,
-          backgroundColor: theme.listHeaderBackgroundColor,
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 3,
-          },
-          shadowOpacity: 0.8,
-          shadowRadius: 4.65,
-          justifyContent: "center",
-          overflow: "hidden",
-          // alignItems: "center",
+          height: height,
+          width: width,
+          top: 0,
+          left: 0,
         }}
       >
         <Pressable
-          onHoverIn={() => setFavBtnBgCol(theme.selectedItemBackgroundColor)}
-          onHoverOut={() => setFavBtnBgCol(theme.listHeaderBackgroundColor)}
-          onPress={() => addToFavorites(server)}
           style={{
-            backgroundColor: favBtnBgCol,
-            paddingLeft: 10,
-            paddingRight: 30,
-            paddingVertical: 7,
+            height: "100%",
+            width: "100%", // @ts-ignore
+            cursor: "default",
+          }}
+          onPress={() => hide()}
+        />
+        <View
+          style={{
+            position: "absolute",
+            top: position.y,
+            left: position.x,
+            borderRadius: 4,
+            backgroundColor: theme.listHeaderBackgroundColor,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.8,
+            shadowRadius: 4.65,
+            justifyContent: "center",
+            overflow: "hidden",
+            // alignItems: "center",
           }}
         >
-          <Text bold color={"white"}>
-            ❤️ Add to Favorites
-          </Text>
-        </Pressable>
+          <Pressable
+            onHoverIn={() => setFavBtnBgCol(theme.selectedItemBackgroundColor)}
+            onHoverOut={() => setFavBtnBgCol(theme.listHeaderBackgroundColor)}
+            onPress={() => {
+              addToFavorites(server);
+              hide();
+            }}
+            style={{
+              backgroundColor: favBtnBgCol,
+              paddingLeft: 10,
+              paddingRight: 30,
+              paddingVertical: 7,
+            }}
+          >
+            <Text bold color={"white"}>
+              ❤️ Add to Favorites
+            </Text>
+          </Pressable>
+        </View>
       </View>
     );
   } else {
