853563f8be9116c425641bb51a00f1f9717b73c5
iAmir <hhm6@yahoo.com>
2023-10-06 16:08:09 +0330

    increase chunk size for js bundle

--- vite.config.ts
+++ vite.config.ts
@@ -53,4 +53,7 @@ export default defineConfig(async () => ({
   // 3. to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    chunkSizeWarningLimit: 600,
+  },
 }));
