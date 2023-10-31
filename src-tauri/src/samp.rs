8db5be12d14b10a8b7ae742cd0a4266cdd537cec
iAmir <hhm6@yahoo.com>
2023-10-31 05:28:10 +0330

    replace parts of the string in gtasa path from samp

--- src-tauri/src/samp.rs
+++ src-tauri/src/samp.rs
@@ -18,16 +18,13 @@ pub fn get_gtasa_path() -> String {
     let key = RegKey::predef(HKEY_CURRENT_USER).open_subkey_with_flags(r"Software\SAMP", KEY_READ);
     match key {
         Ok(s) => {
-            let result = s.get_value("gta_sa_exe");
+            let result: Result<String, _> = s.get_value("gta_sa_exe");
             match result {
-                Ok(val) => {
-                    return val;
-                }
-                Err(_) => return "".to_string(),
+                Ok(val) => val.replace("\\gta_sa.exe", ""),
+                Err(_) => "".to_string(),
             }
-            // val
         }
-        Err(_) => return "".to_string(),
+        Err(_) => "".to_string(),
     }
 }
 
@@ -38,13 +35,10 @@ pub fn get_nickname() -> String {
         Ok(s) => {
             let result = s.get_value("PlayerName");
             match result {
-                Ok(val) => {
-                    return val;
-                }
-                Err(_) => return "".to_string(),
+                Ok(val) => val,
+                Err(_) => "".to_string(),
             }
-            // val
         }
-        Err(_) => return "".to_string(),
+        Err(_) => "".to_string(),
     }
 }
