2c996875c104bd523af46e6ada22c6c1c721570b
iAmir <hhm6@yahoo.com>
2023-10-31 07:29:17 +0330

    ip validation helper func

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -135,3 +135,14 @@ Please refer to https://sa-mp.mp/ to download SA-MP
 
   return true;
 };
+
+export const validateIPaddress = (ipaddress: string) => {
+  if (
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+      ipaddress
+    )
+  ) {
+    return true;
+  }
+  return false;
+};
