e755d859c29dcdc73670174f17f35173aaaa2748
iAmir <hhm6@yahoo.com>
2023-11-12 11:20:13 +0330

    add spanish locale thanks to @AGraber

---
+++ src/locales/translations/es.ts
@@ -0,0 +1,84 @@
+export default {
+  favorites: "Favoritos",
+  internet: "Internet",
+  partners: "Socios",
+  recently_joined: "Servidores recientes",
+  nickname: "Usuario",
+  settings: "Configuración",
+  minimize: "Minimizar",
+  maximize: "Maximizar",
+  close: "Cerrar",
+  add_server_modal_description_1:
+    "Añade manualmente un servidor a la lista de favoritos.",
+  add_server_modal_description_2: "Ejemplo: 127.0.0.1:7777",
+  add: "Añadir",
+  server: "Servidor",
+  address: "Dirección",
+  players: "Jugadores",
+  server_join_prompt_enter_password:
+    "Este servidor está protegido, por favor ingresa la contraseña.",
+  server_join_prompt_enter_password_input_placeholder:
+    "Ingresa la contraseña...",
+  server_join_prompt_nickname_input_placeholder: "Ingresa el usuario...",
+  connect: "Conectar",
+  copy: "Copiar",
+  remove_from_favorites: "Eliminar de favoritos",
+  add_to_favorites: "Agregar a favoritos",
+  filters: "Filtros",
+  filter_only_omp_servers: "Solo servidores open.mp",
+  filter_non_empty_servers: "Servidores no vacíos",
+  filter_unpassworded_servers: "Servidores sin contraseña",
+  rule: "Regla",
+  value: "Valor",
+  player: "Jugador",
+  score: "Puntuación",
+  locked: "Bloqueado",
+  unlocked: "Desbloqueado",
+  openmp_server: "Servidor open.mp",
+  name: "Nombre",
+  ping: "Ping",
+  mode: "Modo",
+  filter_servers: "Filtrar servidores",
+  search_for_server_hostname_mode: "Buscar nombre de host/modo del servidor",
+  clear_recently_joined_list: "Limpiar lista de servidores recientes",
+  refresh_servers: "Actualizar lista de servidores",
+  play: "Jugar",
+  remove_selected_server_from_favorites:
+    "Eliminar servidor seleccionado de favoritos",
+  add_selected_server_to_favorites: "Agregar servidor seleccionado a favoritos",
+  add_server: "Agregar servidor",
+  hide_player_and_rule_list: "Ocultar lista de jugadores y reglas",
+  show_player_and_rule_list: "Mostrar lista de jugadores y reglas",
+  copy_server_info: "Copiar información del servidor",
+  settings_gta_path_input_label:
+    "Ruta de GTA: San Andreas (donde también está instalado SA-MP)",
+  browse: "Explorar",
+  settings_import_nickname_gta_path_from_samp:
+    "Importar usuario y ruta de gtasa desde la configuración de SA-MP",
+  settings_import_samp_favorite_list:
+    "Importar lista de favoritos desde los datos de SA-MP",
+  settings_reset_application_data:
+    "Restablecer datos de la aplicación (borra configuraciones y listas)",
+  settings_new_update_available:
+    "⚠ ¡Nueva actualización disponible! Haz clic para descargar. ⚠",
+  settings_credits_made_by: "Hecho con ❤️ por",
+  settings_credits_view_source_on_github: "Ver código fuente en GitHub",
+  update_modal_update_available_title: "¡Actualización disponible!",
+  update_modal_update_available_description:
+    '¡Hay una nueva versión del launcher disponible!\nTu versión actual del launcher: {{ version }}\nVersión actual del launcher: {{ newVersion }}\nHaz clic en "Descargar" para abrir la página de descarga',
+  download: "Descargar",
+  update_modal_remind_me_next_time: "Recordarme la próxima vez",
+  update_modal_skip_this_update: "Saltar esta actualización",
+  gta_path_modal_cant_find_game_title:
+    "¡No se puede encontrar GTA San Andreas!",
+  gta_path_modal_cant_find_game_description:
+    'No se puede encontrar GTA San Andreas en este directorio:\n  - "{{ path }}"\nNo se puede encontrar "gta_sa.exe" en la ruta proporcionada.',
+  open_settings: "Abrir configuración",
+  cancel: "Cancelar",
+  gta_path_modal_cant_find_samp_title: "¡No se puede encontrar SA-MP!",
+  gta_path_modal_cant_find_samp_description:
+    'No se puede encontrar la instalación de SA-MP en este directorio:\n  - "{{ path }}"\nNo se puede encontrar "samp.dll" en la ruta proporcionada.\n\nSi no tienes SA-MP instalado, puedes descargarlo desde https://sa-mp.mp/ haciendo clic en **Descargar**.',
+  notification_add_to_favorites_title: "¡Añadido a favoritos!",
+  notification_add_to_favorites_description:
+    "{{ server }} ha sido agregado a tu lista de favoritos.",
+};
