439bc2d36adec622774766b18dc8fdb72ec6feb3
Роман <roman.saintrose@gmail.com>
2023-11-24 23:49:39 +0300

    Added a czk language file

---
+++ src/locales/translations/cs.ts
@@ -0,0 +1,93 @@
+export default {
+    favorites: "Vybrané stránky",
+    internet: "Internet",
+    partners: "Partneři",
+    recently_joined: "Nedávné",
+    nickname: "Přezdívka",
+    settings: "Nastavení",
+    minimize: "Sbalit",
+    maximize: "Rozložit",
+    close: "Zavřít",
+    add_server_modal_description_1: "Ruční přidání serveru do seznamu oblíbených položek.",
+    add_server_modal_description_2: "Příklad: 127.0.0.1:7777",
+    add: "Přidat.",
+    server: "Server",
+    address: "Adresa",
+    players: "Hráči",
+    server_join_prompt_enter_password:
+      "Tento server je chráněn heslem, zadejte prosím heslo.",
+    server_join_prompt_enter_password_input_placeholder: "Zadejte své heslo...",
+    server_join_prompt_nickname_input_placeholder: "Zadejte přezdívku...",
+    connect: "Připojení",
+    copy: "Kopírovat",
+    remove_from_favorites: "Odebrat z oblíbených položekх",
+    add_to_favorites: "Přidat do oblíbených",
+    filters: "Filtry",
+    filter_only_omp_servers: "Pouze servery Open.mp",
+    filter_non_empty_servers: "Neprázdné servery",
+    filter_unpassworded_servers: "Servery bez hesla",
+    rule: "Pravidlo",
+    value: "Význam",
+    player: "Hráč",
+    score: "Brýle",
+    locked: "S heslem",
+    unlocked: "Žádné heslo",
+    openmp_server: "open.mp server",
+    name: "Název",
+    ping: "Ping",
+    mode: "Režim",
+    filter_servers: "Servo filtr",
+    search_for_server_hostname_mode: "Vyhledávání podle názvu/režimu serveru",
+    clear_recently_joined_list: "Vymazat nedávný seznam",
+    refresh_servers: "Aktualizace seznamu serverů",
+    play: "Přehrát",
+    remove_selected_server_from_favourites: "Odebrat server z oblíbených",
+    add_selected_server_to_favourites: "Přidat server do oblíbených",
+    add_server: "Přidat server",
+    hide_player_and_rule_list: "Skrýt seznam hráčů a pravidel",
+    show_player_and_rule_list: "Zobrazit seznam hráčů a pravidel",
+    copy_server_info: "Zkopírovat informace",
+    settings_gta_path_input_label:
+      "Cesta ke hře GTA: San Andreas (kde je také nainstalován SA-MP)",
+    browse: "Procházet",
+    settings_import_nickname_gta_path_from_samp:
+      "Importovat přezdívku a cestu ke hře z nastavení SA-MP",
+    settings_import_samp_favourite_list:
+      "Importovat seznam oblíbených položek z dat SA-MP",
+    settings_reset_application_data:
+      "Obnovení dat aplikace (vymazání nastavení a seznamů)",
+    settings_new_update_available:
+      "⚠ Aktualizace je k dispozici. Klikněte pro její stažení! ⚠",
+    settings_credits_made_by: "Made with ❤️ ",
+    settings_credits_view_source_on_github: "Zdrojový kód na GitHubu",
+    update_modal_update_available_title: "Aktualizace je k dispozici!",
+    update_modal_update_available_description:
+      "Vaše verze spouštěče: Aktuální verze spouštěče: {{ version }}}\nAktuální verze spouštěče: {{ newVersion }}}\nKliknutím na 'download' otevřete stránku s verzí",
+    Stáhnout: {{ verze}}
+    update_modal_remind_me_next_time: 'Připomeň mi to později',
+    update_modal_skip_this_update: 'Přeskočit aktualizaci',
+    gta_path_modal_cant_find_game_title: "GTA San Andreas не найдена!",
+    gta_path_modal_cant_find_game_description:
+      'Не удается найти GTA San Andreas в этой папке:\n  - "{{ path }}"\nОтсутствует "gta_sa.exe" по указанному пути.',
+    open_settings: "Открыть настройки",
+    cancel: "Отмена",
+    gta_path_modal_cant_find_samp_title: "SA-MP не найден!",
+    gta_path_modal_cant_find_samp_description:
+      'Не удается найти SA-MP в этой папке:\n  - "{{ path }}"\nОтсутствует "samp.dll" по указанному пути.\n\nЕсли у вас не установлен SA-MP, вы можете загрузить его здесь https://sa-mp.mp/ нажав **Загрузить**.',
+    notification_add_to_favorites_title: "Добавлено в избранные!",
+    notification_add_to_favorites_description:
+      "{{ server }} добавлен в ваш список избранных.",
+    nickname_modal_name_not_set_title: "Не указан никнейм!",
+    nickname_modal_name_not_set_description:
+      "Прежде чем подключиться к серверу, вы должны выбрать себе никнейм.",
+    gta_path_modal_path_not_set_title: "Путь к GTA San Andreas не задан!",
+    gta_path_modal_path_not_set_description:
+      "Вы не указали путь к GTA San Andreas, зайдите в настройки и найдите папку с игрой.",
+    admin_permissions_required_modal_title: "Требуются права администратора!",
+    admin_permissions_required_modal_description:
+      'Похоже, что для запуска GTA: San Andreas требуются права администратора. Это может произойти по разным причинам, например, если ваша игра установлена на диске "C". Пожалуйста, откройте open.mp лаунчер повторно от имени администратора с помощью кнопки "Запуск от имени администратора" либо вручную',
+    run_as_admin: "Запуск от имени администратора",
+  };
+  
+
+
