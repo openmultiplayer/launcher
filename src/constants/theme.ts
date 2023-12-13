3e7a764d37e739d823e62774aa9028817e82eb6c
iAmir <hhm6@yahoo.com>
2023-12-13 07:33:15 +0330

    light theme colors

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -22,11 +22,11 @@ export const darkThemeColors: ThemeColors = {
 
 export const lightThemeColors: ThemeColors = {
   primary: "#897AF1",
-  secondary: "#1A1A1E",
-  textPrimary: "#FAFAFA",
-  textSecondary: "#FFFFFF40",
-  textPlaceholder: "#909090",
-  itemBackgroundColor: "#222227",
-  textInputBackgroundColor: "#16161A",
-  serverListItemBackgroundColor: "#303038",
+  secondary: "#FFFFFF",
+  textPrimary: "#1A1A1E",
+  textSecondary: "#1A1A1E80",
+  textPlaceholder: "#6D7071",
+  itemBackgroundColor: "#F3F6FC",
+  textInputBackgroundColor: "#F3F6FC",
+  serverListItemBackgroundColor: "#C5BEF8",
 };
