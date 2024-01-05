dd55e4776690bd1b93a2f4d2f590dc31c07a3b5a
iAmir <hhm6@yahoo.com>
2024-01-05 06:18:05 +0330

    cover resize mode for banners

--- src/containers/MainBody/ServerInfo/index.tsx
+++ src/containers/MainBody/ServerInfo/index.tsx
@@ -98,6 +98,7 @@ const ServerInfo = () => {
                   height: "100%",
                   width: "100%",
                 }}
+                resizeMode="cover"
               />
             ) : (
               <>
