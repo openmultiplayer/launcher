08718e20d59e03b51e6a3fd4cc85f877bce6fc02
Lasha <silly4441@gmail.com>
2024-10-24 05:32:46 +0400

    added georgian lang

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -26,6 +26,7 @@ import Sindhi from "./translations/sn";
 import Vietnamese from "./translations/vi";
 import Tamil from "./translations/ta";
 import Ukrainian from "./translations/ua";
+import Georgian from "./translations/ge";
 
 export type LanguageType =
   | "en"
@@ -53,7 +54,8 @@ export type LanguageType =
   | "sn"
   | "vi"
   | "ta"
-  | "ua";
+  | "ua"
+  | "ge";
 
 export const getLanguages = (): {
   [x: string]: {
@@ -192,6 +194,11 @@ export const getLanguages = (): {
       label: "Українська",
       type: "ua",
       translations: Ukrainian,
+    },
+    "ge": {
+      label: "ქართული",
+      type: "ge",
+      translations: Georgian,
     }
   };
 };
